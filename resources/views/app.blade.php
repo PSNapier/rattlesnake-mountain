438be328b7201633f346ab33133af4abd9c4b721
PSNapier <46052087+PSNapier@users.noreply.github.com>
2025-05-13 09:39:51 -0500

    feat: transfering theme, page layout and working on nav styling

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}"  @class(['dark' => ($appearance ?? 'system') == 'dark'])>
+{{-- <html lang="{{ str_replace('_', '-', app()->getLocale()) }}"  @class(['dark' => ($appearance ?? 'system') == 'dark'])> --}}
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         {{-- Inline script to detect system dark mode preference and apply it immediately --}}
-        <script>
+        {{-- <script>
             (function() {
                 const appearance = '{{ $appearance ?? "system" }}';
 
@@ -17,10 +18,10 @@
                     }
                 }
             })();
-        </script>
+        </script> --}}
 
         {{-- Inline style to set the HTML background color based on our theme in app.css --}}
-        <style>
+        {{-- <style>
             html {
                 background-color: oklch(1 0 0);
             }
@@ -28,22 +29,24 @@
             html.dark {
                 background-color: oklch(0.145 0 0);
             }
-        </style>
+        </style> --}}
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
-        <link rel="icon" href="/favicon.ico" sizes="any">
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml">
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+        {{-- <link rel="icon" href="/favicon.ico" sizes="any"> --}}
+        {{-- <link rel="icon" href="/favicon.svg" type="image/svg+xml"> --}}
+        {{-- <link rel="apple-touch-icon" href="/apple-touch-icon.png"> --}}
+        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🐍</text></svg>">
 
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+        <link href="https://fonts.bunny.net/css?family=amaranth:400,400i,700,700i" rel="stylesheet" />
 
         @routes
         @vite(['resources/js/app.ts', "resources/js/pages/{$page['component']}.vue"])
         @inertiaHead
     </head>
-    <body class="font-sans antialiased">
+    <body class="font-sans antialiased bg-cape-palliser-100 text-cape-palliser-600">
         @inertia
     </body>
 </html>
