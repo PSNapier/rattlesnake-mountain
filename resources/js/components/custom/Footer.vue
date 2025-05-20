dd011c9289cbeb4f218a1c7248bab69935f57744
PSNapier <46052087+PSNapier@users.noreply.github.com>
2025-05-20 13:39:19 -0500

    feat: moved HeaderNav and Footer to their own components, messing with eslint & prettier

---
+++ resources/js/components/custom/Footer.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+import { linkDict } from '@/composables/useLinkDictionary';
+</script>
+
+<template>
+	<footer class="max-container text-mine-shaft-600 p-4 text-center text-sm">
+		<p>
+			Rattlesnake Mountain is a horse art roleplay game by <a href=" {{ linkDict.OWNER.path }}"> {{ linkDict.OWNER.label }}</a>
+		</p>
+		<p>Copyright © Siat-s {{ new Date().getFullYear() }}</p>
+		<p>Website by <a href="#">Abature Studio</a></p>
+	</footer>
+</template>
---
+++ resources/js/composables/useLinkDictionary.ts
@@ -0,0 +1,14 @@
+function makeLink(label: string, path: string) {
+	return {
+		label,
+		path,
+	};
+}
+
+export const linkDict = {
+	HOME: makeLink('Home', '/'),
+	GETTING_STARTED: makeLink('Getting Started', '/getting-started'),
+	WILDLIFE: makeLink('Wildlife', '/wildlife'),
+	CONTACT_US: makeLink('Contact US', '/contact-us'),
+	OWNER: makeLink('Siat-s', 'https://siat-s.deviantart.com'),
+};
