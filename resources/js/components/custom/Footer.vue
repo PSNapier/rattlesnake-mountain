e162754e4c508cac00314aecfa4a9d8af7842dcc
PSNapier <46052087+PSNapier@users.noreply.github.com>
2025-06-11 18:44:37 -0500

    feat: populate HeaderNav

--- resources/js/components/custom/Footer.vue
+++ resources/js/components/custom/Footer.vue
@@ -5,7 +5,10 @@ import { linkDict } from '@/composables/useLinkDictionary';
 <template>
 	<footer class="max-container text-mine-shaft-600 p-4 text-center text-sm">
 		<p>
-			Rattlesnake Mountain is a horse art roleplay game by <a href=" {{ linkDict.OWNER.path }}"> {{ linkDict.OWNER.label }}</a>
+			Rattlesnake Mountain is a horse art roleplay game by
+			<a href=" {{ linkDict.OWNER.path }}">
+				{{ linkDict.OWNER.label }}</a
+			>
 		</p>
 		<p>Copyright © Siat-s {{ new Date().getFullYear() }}</p>
 		<p>Website by <a href="#">Abature Studio</a></p>
