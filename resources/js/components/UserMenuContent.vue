a5fda061bcc27ec71f2885fed5fc70ce18f18adc
PSNapier <arjones.tx@gmail.com>
2025-09-03 17:00:33 -0500

    feat: added dashboard link

--- resources/js/components/UserMenuContent.vue
+++ resources/js/components/UserMenuContent.vue
@@ -1,9 +1,14 @@
 <script setup lang="ts">
 import UserInfo from '@/components/UserInfo.vue';
-import { DropdownMenuGroup, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator } from '@/components/ui/dropdown-menu';
+import {
+	DropdownMenuGroup,
+	DropdownMenuItem,
+	DropdownMenuLabel,
+	DropdownMenuSeparator,
+} from '@/components/ui/dropdown-menu';
 import type { User } from '@/types';
 import { Link, router } from '@inertiajs/vue3';
-import { LogOut, Settings } from 'lucide-vue-next';
+import { Home, LogOut, Settings } from 'lucide-vue-next';
 
 interface Props {
 	user: User;
@@ -21,8 +26,7 @@ defineProps<Props>();
 		<div class="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
 			<UserInfo
 				:user="user"
-				:show-email="true"
-			/>
+				:show-email="true" />
 		</div>
 	</DropdownMenuLabel>
 	<DropdownMenuSeparator />
@@ -30,10 +34,19 @@ defineProps<Props>();
 		<DropdownMenuItem :as-child="true">
 			<Link
 				class="block w-full"
+				:href="route('dashboard')"
+				prefetch
+				as="button">
+				<Home class="mr-2 h-4 w-4" />
+				Dashboard
+			</Link>
+		</DropdownMenuItem>
+		<DropdownMenuItem :as-child="true">
+			<Link
+				class="block w-full"
 				:href="route('profile.edit')"
 				prefetch
-				as="button"
-			>
+				as="button">
 				<Settings class="mr-2 h-4 w-4" />
 				Settings
 			</Link>
@@ -46,8 +59,7 @@ defineProps<Props>();
 			method="post"
 			:href="route('logout')"
 			@click="handleLogout"
-			as="button"
-		>
+			as="button">
 			<LogOut class="mr-2 h-4 w-4" />
 			Log out
 		</Link>
