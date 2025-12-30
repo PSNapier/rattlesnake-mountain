3b88285e60ee656672c9fb477a89d6262e5f623b
PSNapier <arjones.tx@gmail.com>
2025-12-30 06:37:13 -0600

    fix: title on admin page

--- resources/js/pages/admin/Index.vue
+++ resources/js/pages/admin/Index.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
 import AppLayout from '@/layouts/AppLayout.vue';
+import { Head } from '@inertiajs/vue3';
 </script>
 
 <template>
+	<Head title="Admin" />
+
 	<AppLayout>
 		<div class="p-6">
 			<h1 class="text-3xl font-bold">Admin</h1>
